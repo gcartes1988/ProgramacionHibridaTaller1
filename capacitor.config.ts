fa8b0cdf739e611737135366595e280b9c95704c
gcartes1988 <gustavocartes@gmail.com>
2024-06-04 20:31:02 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.gustavocartestaller1',
+  appName: 'gustavocartestaller1',
+  webDir: 'www'
+};
+
+export default config;
